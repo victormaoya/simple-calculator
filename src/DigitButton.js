feb4bc34aa0f7427ebb2ad5465a76ff8f9c8f37a
Victor Maoya <victormaoya@gmail.com>
2023-06-08 10:03:39 +0300

    A simple calculator using react

--- src/DigitButton.js
+++ src/DigitButton.js
@@ -1,7 +1,11 @@
-import ACTIONS from './App';
+import { ACTIONS } from "./App"
 
 export default function DigitButton({ dispatch, digit }) {
-    return <button onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}>
-    {digit}
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
     </button>
+  )
 }
